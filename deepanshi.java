003eff886b3f836e6a031eca70f72459500977a5
DeepanshiAhuja <101317376+DeepanshiAhuja@users.noreply.github.com>
2022-03-10 18:00:21 +0530

    Add files via upload

---
+++ deepanshi.java
@@ -0,0 +1,9 @@
+class HelloWorld
+{
+   public static void main(String[]args)
+       
+         {
+                String name="John";
+                System.out.println("Hello + name);
+      }
+}
